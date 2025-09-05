a94a4e6c1133c99e6a15dc4f3bc50b47f80f4304
Mahima136 <pjmahima136@gmail.com>
2025-09-05 14:39:06 +0530

    Add files via upload

---
+++ Neon.java
@@ -0,0 +1,25 @@
+
+
+import java.util.Scanner;
+
+public class Neon {
+	public static void main(String[]args) {
+		int sumSquareDigits = 0,num;
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Enter a number:");
+		num = scanner.nextInt();
+		scanner.close();
+		int square = num*num;
+		while(square != 0)
+		{
+			int lastDigit = square % 10;
+			sumSquareDigits = sumSquareDigits + lastDigit;
+			square = square / 10;
+		}
+		if(num == sumSquareDigits)
+			System.out.println(num + " is a Neon Number.");
+		else
+			System.out.println(num + " is not a Neon Number.");
+	}
+
+}
